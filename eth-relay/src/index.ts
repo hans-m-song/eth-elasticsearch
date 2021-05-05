fc7ae2272717413aefd60e273cff14035216eea7
hans-m-song <hans.m.song@gmail.com>
2021-05-05 16:38:23 +1000

    consuming env vars

--- eth-relay/src/index.ts
+++ eth-relay/src/index.ts
@@ -1,1 +1,15 @@
 import 'source-map-support/register'
+import { constants } from './constants';
+import Web3 from 'web3';
+
+const { ETH_RPC_ADDR, ELASTICSEARCH_ADDR } = constants
+
+
+console.log(`eth-relay listening to "${ETH_RPC_ADDR}" and sending to "${ELASTICSEARCH_ADDR}"`);
+
+
+(async () => {
+  const web3 = new Web3(ETH_RPC_ADDR)
+  const blockid = await web3.eth.getBlockNumber();
+  console.log(blockid)
+})()
